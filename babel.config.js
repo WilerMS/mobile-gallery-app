11d9a7905c196c687ec89a405b5976826f25afc5
WilerMS <wilersoriano@gmail.com>
2024-01-06 07:48:06 +0100

    add ts paths aliases

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+       'module-resolver',
+       {
+         root: ['./src'],
+         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+         alias: {
+          "@": "./src",
+          "@components": "./src/components",
+          "@screens": "./src/screens/index",
+          "@api": "./src/api"
+         }
+       }
+      ]
+    ]
+  }
+}
