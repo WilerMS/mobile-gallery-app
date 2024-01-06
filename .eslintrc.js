19fbb65dd19ff635709a96ab0081d8e222a0f2f3
WilerMS <wilersoriano@gmail.com>
2024-01-06 09:44:56 +0100

    Add

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,6 @@ module.exports = {
       "@typescript-eslint/no-floating-promises": "off",
       "@typescript-eslint/strict-boolean-expressions": "off",
       "@typescript-eslint/no-empty-interface": "off",
-      "@typescript-eslint/ban-ts-comment": "0ff"
+      "@typescript-eslint/ban-ts-comment": "off"
     }
 }
