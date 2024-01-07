acefda4a1924d4d3d2a725c006cecb7753069224
WilerMS <wilersoriano@gmail.com>
2024-01-07 05:57:12 +0100

    Add pexels and pixabay logic to return photos

--- src/api/pexels.ts
+++ src/api/pexels.ts
@@ -1,25 +1,54 @@
 import axios from 'axios'
-import { type ImagesResponse } from './pexels.d'
-import { API_KEY } from '../constants/env'
+import { type PexelsImage, type PexelsResponse } from './pexels.d'
+import { PEXELS_API_KEY, PEXELS_API_URL } from '../constants/env'
+import { type Photo } from './common.d'
+// @ts-expect-error
+import female from '@/assets/female.png'
 
-export const searchImages = async (q = '') => {
-  return await axios.get<ImagesResponse>(
-    `https://api.pexels.com/v1/search?query=${q}&per_page=40`,
-    {
-      headers: {
-        Authorization: API_KEY
-      }
+const convertResponse = (photos: PexelsImage[]): Photo[] => {
+  return photos.map((photo) => {
+    return {
+      id: photo.id,
+      type: 'photo',
+      height: photo.height,
+      width: photo.width,
+      preview: photo.src.medium,
+      url: photo.src.original,
+      user: photo.photographer,
+      userImage: female,
+      tags: [],
+      likes: 0,
+      collections: 0,
+      comments: 0,
+      downloads: 0,
+      size: 0,
+      views: 0
     }
-  )
+  })
 }
 
-export const loadCuratedImages = async (q = '') => {
-  return await axios.get<ImagesResponse>(
-    'https://api.pexels.com/v1/curated?per_page=40',
+export const searchImages = async (q = ''): Promise<Photo[]> => {
+  const res = await axios.get<PexelsResponse>(
+    `${PEXELS_API_URL}/search?query=${q}&per_page=40`,
     {
       headers: {
-        Authorization: API_KEY
+        Authorization: PEXELS_API_KEY
       }
     }
   )
+
+  return convertResponse(res.data.photos)
+}
+
+export const loadCuratedImages = async (): Promise<Photo[]> => {
+  const res = await axios.get<PexelsResponse>(
+    `${PEXELS_API_URL}/curated?per_page=40`,
+    {
+      headers: {
+        Authorization: PEXELS_API_KEY
+      }
+    }
+  )
+
+  return convertResponse(res.data.photos)
 }
---
+++ src/api/pixabay.ts
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import { type PixabayImage, type PixabayResponse } from './pìxabay.d'
+import { PIXABAY_API_KEY, PIXABAY_API_URL } from '../constants/env'
+import { type Photo } from './common.d'
+
+const convertResponse = (photos: PixabayImage[]): Photo[] => {
+  return photos.map((photo) => {
+    return {
+      id: photo.id,
+      type: 'photo',
+      height: photo.imageHeight,
+      width: photo.imageWidth,
+      size: photo.imageSize,
+      preview: photo.previewURL,
+      url: photo.largeImageURL,
+      user: photo.user,
+      userImage: photo.userImageURL,
+      tags: photo.tags.split(','),
+      likes: photo.likes,
+      collections: photo.collections,
+      comments: photo.comments,
+      downloads: photo.downloads,
+      views: photo.views
+    }
+  })
+}
+
+export const searchPixabayImages = async (q = '') => {
+  const res = await axios.get<PixabayResponse>(
+    `${PIXABAY_API_URL}/search?q=${q}&per_page=40&key=${PIXABAY_API_KEY}`
+  )
+  return convertResponse(res.data.hits)
+}
+
+export const loadPixabayImages = async (q = '') => {
+  const res = await axios.get<PixabayResponse>(
+    `${PIXABAY_API_URL}/search?per_page=40&key=${PIXABAY_API_KEY}`
+  )
+  return convertResponse(res.data.hits)
+}
